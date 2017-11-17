c7ec470d73565dd9035e8072039cf19213820bfd
David Hale <david.hale7@gmail.com>
2017-11-17 10:20:47 -0800

    add validation for content length and spec

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   validates :author, :presence => true
   validates :content, :presence => true
   validates_inclusion_of :rating, in: 1..5
+  validates_length_of :content, :within => 50..250
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,5 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content }
   it { should validate_inclusion_of(:rating).in_range(1..5) }
+  it { should validate_length_of(:content).is_at_least(50).is_at_most(250) }
 end
