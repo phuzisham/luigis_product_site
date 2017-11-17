7405d720f0e269083281a822cc68a8c01a1578bc
Guest User <Guest@Epicodus-1A.local>
2017-11-17 12:00:02 -0800

    add product view

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,28 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added!"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:title)
+  end
+end
