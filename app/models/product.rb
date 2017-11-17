d3c31769d97b8eedb3ad783f63e8e038444b2de5
Guest User <Guest@Epicodus-1A.local>
2017-11-17 13:41:31 -0800

    seed products and reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews
-  acts_as_votable
+  # acts_as_votable
   validates :title, :presence => true
   validates :country, :presence => true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+product_list = [
+  ["Jazz", 20, "America"],
+  ["Rock", 14, "Europe"],
+  ["Mongolian Throating", 32, "Mongolia"],
+  ["Human", 56, "Africa"],
+  ["Rich", 17, "Dubai"],
+  ["Poor", 40, "Istanbul"]
+]
+
+#seeding products
+product_list.each do |title, cost, country|
+  Product.create( title: title, cost: cost, country: country )
+end
+
+p "Created #{Product.count} products"
+
+#seeding reviews
+50.times do
+  Review.create!(author: Faker::RockBand.unique.name,
+                product_id: Faker::Number.between(Product.first.id, Product.last.id),
+                content: Faker::Hipster.unique.sentence(8),
+                rating: Faker::Number.between(1, 5))
+end
+
+p "Created #{Review.count} reviews"
