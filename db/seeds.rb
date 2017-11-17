f5fcca3b1073e5693c2333c553862709e1bffc30
David Hale <david.hale7@gmail.com>
2017-11-17 14:07:51 -0800

    seeding required amount

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,21 @@
 Product.destroy_all
 Review.destroy_all
 
-product_list = [
-  ["Jazz", 20, "America"],
-  ["Rock", 14, "Europe"],
-  ["Mongolian Throating", 32, "Mongolia"],
-  ["Human", 56, "Africa"],
-  ["Rich", 17, "Dubai"],
-  ["Poor", 40, "Istanbul"]
-]
-
 #seeding products
-product_list.each do |title, cost, country|
-  Product.create( title: title, cost: cost, country: country )
+50.times do
+  Product.create( title: Faker::Beer.unique.name,
+                  cost: Faker::Number.between(5, 500),
+                  country: Faker::RickAndMorty.location )
 end
 
 p "Created #{Product.count} products"
 
 #seeding reviews
-50.times do
-  Review.create!(author: Faker::RockBand.unique.name,
-                product_id: Faker::Number.between(Product.first.id, Product.last.id),
-                content: Faker::Hipster.unique.sentence(8),
-                rating: Faker::Number.between(1, 5))
+250.times do
+  Review.create!(author: Faker::Simpsons.character,
+                 product_id: Faker::Number.between(Product.first.id, Product.last.id),
+                 content: Faker::Lorem.characters(50..250),
+                 rating: Faker::Number.between(1, 5))
 end
 
 p "Created #{Review.count} reviews"
