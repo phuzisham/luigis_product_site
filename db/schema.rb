fd8cbd1d45145d9abf1c883754ca6a7a49f6cbe7
David Hale <david.hale7@gmail.com>
2017-11-17 10:05:44 -0800

    relation and validation spec for product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117174123) do
+ActiveRecord::Schema.define(version: 20171117180247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171117174123) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "product_id"
   end
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-describe Album do
-  it { should validate_presence_of :title}
-  it { should validate_presence_of :artist_id}
-  it { should belong_to :artist }
+describe Product do
   it { should have_many :reviews }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :country }
 end
