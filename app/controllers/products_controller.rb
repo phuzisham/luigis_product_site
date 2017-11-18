e3aaefd280a4f8aa8fc200d916969837f9667bc2
David Hale <david.hale7@gmail.com>
2017-11-17 16:17:00 -0800

    all crud functionality in place

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,20 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -25,6 +25,22 @@ class ReviewsController < ApplicationController
     @reviews = @product.reviews
   end
 
+  def edit
+    @review = Review.find(params[:id])
+    @product = Product.find(params[:product_id])
+    render :edit
+  end
+
+  def update
+    @review= Review.find(params[:id])
+    @product = Product.find(params[:product_id])
+    if @review.update(review_params)
+      redirect_to product_reviews_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
