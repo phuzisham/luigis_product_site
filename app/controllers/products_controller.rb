2b1955304b035f51ac821008e282b8e70841f176
David Hale <david.hale7@gmail.com>
2017-11-17 16:06:10 -0800

    add delete routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 private
   def product_params
     params.require(:product).permit(:title, :cost, :country)
