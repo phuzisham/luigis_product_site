08c05464381781b6516c435a8fe68a48d7cb4ba6
David Hale <david.hale7@gmail.com>
2017-11-27 13:18:33 -0800

    add img folder

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,4 @@ describe User do
   it { validate_confirmation_of :password }
   it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
-  # it { should validate_presence_of :cost }
 end
