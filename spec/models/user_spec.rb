dd50b66f62343b51b971aea7a9a64f9972e45e03
David Hale <david.hale7@gmail.com>
2017-11-27 11:53:51 -0800

    add user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User do
+  it { validate_confirmation_of :password }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  # it { should validate_presence_of :cost }
+end
