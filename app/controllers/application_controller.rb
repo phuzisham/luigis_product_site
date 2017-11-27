da24c6ac4f60d7157004196518947af0b67f99e6
David Hale <david.hale7@gmail.com>
2017-11-27 09:09:30 -0800

    add sign up and sign in fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
     @topThree = Product.topThree.limit(3)
     @featured = Product.featured.first
   end
+
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
 end
